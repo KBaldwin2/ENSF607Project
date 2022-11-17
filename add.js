f57f7503fa7a1c129ce682f473a9d608508f114a
kbaldwin2 <kimberley.baldwin@ucalgary.ca>
2022-11-17 11:40:08 -0700

    working on courseList and courseSection display

---
+++ add.js
@@ -0,0 +1,6 @@
+console.log("add.js");
+
+//Receives course information entered and sends it to server
+    //Two possible messages received back:
+    //1. Success! Course has been added.
+    //2. Cannot add course - student already in 6 courses.
--- index.js
+++ index.js
@@ -3,6 +3,52 @@
 console.log('index');
 
 //TODO: Figure out how to get the hard coded IP out of this
+//Load new html address
 function load_page(htmlFile) {
     window.location.href = "http://127.0.0.1:5500/ENSF607Project/"+htmlFile;
 }
+
+//TODO Pull all courses from server
+function load_course_list() {
+    //the purpose of this function will be to pull all the courses from the database 
+    //format them as a dictionary {courseId1: [section1, section2], courseId2: [sec1, 2, 3], courseId3: [1]}
+    course_list = {"ENE612": [1, 2, 4], "ENG311": [1,2], "ENSF609": [1,2,3,4], "ENE222": [1]};
+    return course_list;
+}
+
+//Create list of course Ids
+function create_course_list() {
+    let idList = [];
+
+    for (key in load_course_list()) {
+        idList.push(key);
+    }   
+
+    console.log(idList);
+    return idList;
+}
+
+function create_section_list(courseId) {
+    let sections = load_course_list().courseId;
+    return sections;
+}
+
+//Format dropdown menus with courseIds
+
+idList = create_course_list();
+let courseIds = document.getElementById('courseIdList'), id=idList;
+
+//Iterate through courseIds list and add each as an option in form
+for(let i = 0; i < idList.length; i++) {
+    let option = document.createElement("OPTION"),
+                txt = document.createTextNode(idList[i]);
+    option.appendChild(txt);
+    courseIds.insertBefore(option, courseIds.lastChild);
+}
+
+//Filter to the sections offered for specific course
+//TODO: Use add.js action event handler to read what course was selected **hardcoded for testing
+sectionList = create_section_list("ENSF609");
+
+
+//Format dropdown menus with sections - this needs to be dependent on courseIds
